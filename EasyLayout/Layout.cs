00dc6f2a1f606537a2a37fea0caafefd2e04095f
Frank A. Krueger <fak@praeclarum.org>
2019-08-19 11:48:26 -0700

    Add License

--- EasyLayout/Layout.cs
+++ EasyLayout/Layout.cs
@@ -1,5 +1,7 @@
 //
-// Copyright (c) 2013-2019 Frank A. Krueger
+// Copyright (c) Krueger Systems, Inc.
+//
+// All right reserved.
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
